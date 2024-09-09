7da468e4d8dba7fdef9e61df25384ca1b1815e64
IrynaKryvenko <kryvenkoirina93@gmail.com>
2024-09-09 18:28:04 +0300

    sum

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,18 @@
 import { IoCall, IoPerson } from 'react-icons/io5';
 import css from './Contact.module.css'; 
+import { useDispatch } from "react-redux";
+import { deleteContact } from '../../redux/contactsSlice';
 
 const ContactItem = ({ id, name, number, onDelete }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(deleteContact(id));
   return (
       <li className={css.contactItem}>
           <div className={css.contactDetails}>
               <p className={css.contactName}><IoPerson className={css.icon} /> {name}</p>
           <p className={css.contactNumber}><IoCall className={css.icon} /> {number}</p>
           </div>
-          <button className={css.deleteButton} type='button' onClick={() => onDelete(id)}>Delete</button>
+          <button className={css.deleteButton} type='button' onClick={handleDelete}>Delete</button>
     </li>
   );
 };
